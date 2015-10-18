3c4e410216d618e2657553f9ffb7052e5110b487
Guema <floorent1993@gmail.com>
2015-10-18 19:32:10 +0200

    Creating Unit Script

--- Scripts/Unit.cs
+++ Scripts/Unit.cs
@@ -1,15 +1,32 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
+[SelectionBase]
+[DisallowMultipleComponent]
 public class Unit : MonoBehaviour {
 
+    [SerializeField]
+    string faction;
+    [SerializeField]
+    int maxHeath = 100;
+    [SerializeField]
+    int health;
+
+    // This function is called on Component Placement/Replacement
+    void Reset()
+    {
+
+    }
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 }
