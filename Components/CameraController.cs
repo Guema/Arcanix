079b60eb6dc28d569f66059de5ff031ae2b9bea7
Guema <floorent1993@gmail.com>
2016-02-05 00:23:10 +0100

    Adding a mode and temporary exemple code to CameraController.

--- Components/CameraController.cs
+++ Components/CameraController.cs
@@ -1,34 +1,39 @@
 using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
 public class CameraController : MonoBehaviour
 {
     enum ECameraMode
     {
         FPS,
-        TPS
+        TPS,
+        RTS
     }
+    [SerializeField]
+    Transform cameraPointOfView;
+    new Camera camera;
     [SerializeField]
     bool lockX;
     [SerializeField]
     bool lockY;
-
-    [SerializeField]
-    new Camera camera;
+    float zoom;
+    
     [SerializeField]
     ECameraMode cameraMode;
 
     void Reset()
     {
-
+        
     }
-
 
 	// Use this for initialization
 	void Start ()
     {
-
-	}
+        camera = GetComponent<Camera>();
+        zoom = (cameraPointOfView.transform.position - transform.position).magnitude;
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -36,6 +41,17 @@ public class CameraController : MonoBehaviour
         if (cameraMode == ECameraMode.FPS)
         {
             camera.transform.rotation *= Quaternion.Euler(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0.0f);
+        }
+        else if (cameraMode == ECameraMode.TPS)
+        {
+
+        }
+        else if (cameraMode == ECameraMode.RTS)
+        {
+            camera.transform.LookAt(cameraPointOfView);
+            Vector3 vec = (cameraPointOfView.transform.position - transform.position);
+            vec *= Input.GetAxis("Mouse ScrollWheel");
+            transform.position += vec;
         }
     }
 }
