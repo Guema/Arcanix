8b1ec02f30ec551afaf84f44326f7a02089d7845
Florent Quesselaire <floorent1993@gmail.com>
2022-09-15 03:25:10 +0200

    Starting work inside Unity

---
+++ Runtime/Components/UnitScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arcanix
+{
+
+    [AddComponentMenu("Arcanix/Unit")]
+    [SelectionBase, DisallowMultipleComponent]
+    public class UnitScript : MonoBehaviour
+    {
+
+    }
+}
